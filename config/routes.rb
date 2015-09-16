3ea848196ba75b01d78daadbfe87ca24747b5da9
dvanetten03 <diane.vanetten@gmail.com>
2015-09-16 19:04:14 +0000

    Set up initial controller and static landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Chessgame::Application.routes.draw do
+  root 'landing_pages#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
