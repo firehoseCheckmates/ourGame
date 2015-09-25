6c41f54a900a43130a835f0faef3163e3803f477
Anna <annagaiel@gmail.com>
2015-09-25 21:45:26 +0000

    Run the migration after pulling the master

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150923230221) do
+ActiveRecord::Schema.define(version: 20150924183144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: true do |t|
+    t.integer "white_player_id"
+    t.integer "black_player_id"
+  end
+
+  create_table "pieces", force: true do |t|
+    t.integer "row_position"
+    t.integer "col_position"
+    t.boolean "alive"
+    t.string  "color"
+    t.string  "piece_type"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -29,6 +42,7 @@ ActiveRecord::Schema.define(version: 20150923230221) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.string   "provider"
     t.string   "uid"
   end
