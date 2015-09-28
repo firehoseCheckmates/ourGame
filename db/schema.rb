f65efdd4d45bcddfff8a2925bb54aad8f0013dd2
david <davidslee101@gmail.com>
2015-09-28 17:00:52 +0000

    rake db:migrate has been run on this branch

--- db/schema.rb
+++ db/schema.rb
@@ -42,9 +42,9 @@ ActiveRecord::Schema.define(version: 20150924183144) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.string   "provider"
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
