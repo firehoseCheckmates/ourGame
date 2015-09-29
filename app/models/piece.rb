62b713a3c323f77c041aa3c9deab001385251e32
kirubel <kirubel.aklilu@gmail.com>
2015-09-29 01:27:43 +0000

    Added is_obstruced boiler plate code

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,11 @@
 class Piece < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :game
+  belongs_to :user
+  belongs_to :game
+
+  def is_obstructed?(destination_x, destination_y)
+    #to write a method that tests if a piece move is obstructed or not
+    false
+
+  end
 end
+
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -4,4 +4,8 @@ class PieceTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "returns false if no obstructions" do
+  	piece=FactoryGirl.create :piece
+    assert_equal(piece.is_obstructed?(0,1), false) 
+  end
 end
