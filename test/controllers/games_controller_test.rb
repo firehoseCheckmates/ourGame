6ba7dd687b56e0a13bffa3f29eae0eb82deca6ab
david <davidslee101@gmail.com>
2015-10-01 21:06:05 +0000

    wrote some tests in game controller

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class GamesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "game show page" do
+    game = FactoryGirl.create(:game)
+    get :show, :id => game.id
+    assert_response :success
+  end
+
+  test "game new page" do
+    game = FactoryGirl.create(:game)
+    get :new, :id => game.id
+    assert_response :success
+  end
 end
