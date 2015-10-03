09c1625cc791df51a6942405dbc9c6b5e41ff837
Anna <annagaiel@gmail.com>
2015-10-03 17:58:14 +0000

    Fix the create game and move the index method

--- app/models/game.rb
+++ app/models/game.rb
@@ -5,16 +5,6 @@ class Game < ActiveRecord::Base
 
 	after_create :populate_board!
 
-
-	def index
-		@list_all_games=Game.all
-	end
-
-	def new
-		@game=Game.new
-	end
-
-
 	def populate_board!
 		# populate white front row with Pawns
 		(0..7).each do |n|
