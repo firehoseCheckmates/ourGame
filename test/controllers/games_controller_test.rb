e17394a3ada35a681ae309e3070a66104a83ff6d
david <davidslee101@gmail.com>
2015-10-07 22:32:05 +0000

    add tests to game controller

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -4,11 +4,17 @@ class GamesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
-  test 'user_id' do
+  test 'create_game' do
     user = FactoryGirl.create(:user)
     game = FactoryGirl.create(:game, white_player_id: user.id)
     assert true
+  end
 
+  test 'join_game' do
+    sign_in_user = FactoryGirl.create(:sign_in_user, black_player_id: sign_in_user.id)
+    game = Game.last
+    game.white_player_id == user.id
+    game.black_player_id == sign_in_user.id
   end
 
 
