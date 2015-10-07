d3b41bcfb243ddbfd79ff5d0778bb782fff21080
david <davidslee101@gmail.com>
2015-10-07 20:07:49 +0000

    create update method in games controller

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -4,4 +4,13 @@ class GamesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'user_id' do
+    user = FactoryGirl.create(:user)
+    game = FactoryGirl.create(:game, white_player_id: user.id)
+    assert true
+
+  end
+
+
+
 end
