afee71f8c16d5ab795d070f57ec1f705cc133d7f
david <davidslee101@gmail.com>
2015-10-09 03:45:08 +0000

    add test but needs work

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -4,20 +4,11 @@ class GamesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
-  # test 'create_game' do
-  #   user = FactoryGirl.create(:user)
-  #   game = FactoryGirl.create(:game, white_player_id: user.id)
-  #   assert true
-  # end
 
-  # test 'join_game' do
-  #   sign_in_user = FactoryGirl.create(:sign_in_user, black_player_id: sign_in_user.id)
-  #   game = Game.last
-  #   game.white_player_id == user.id
-  #   game.black_player_id == sign_in_user.id
-  #   assert true
-  # end
-
-
+  test "should get index" do
+    get :index
+    assert_response 302
+    # assert_not_nil assigns(:articles)
+  end
 
 end
