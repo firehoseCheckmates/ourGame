73c341a1a150b7e7360b2801699749f880ec2813
annagaiel <annagaiel@gmail.com>
2015-10-15 13:52:57 +0000

    Refactor assign_piece class of color and type

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -8,31 +8,6 @@ module GamesHelper
   end
 
   def assign_piece(type, color)
-    case
-    when type == 'Rook' && color == 'Black'
-      "black-rook"
-    when type == 'Knight' && color == 'Black'
-      "black-knight"
-    when type == 'Bishop' && color == 'Black'
-      "black-bishop"
-    when type == 'Queen' && color == 'Black'
-      "black-queen"
-    when type == 'King' && color == 'Black'
-      "black-king"
-    when type == 'Pawn' && color == 'Black'
-      "black-pawn"
-    when type == 'Rook' && color == 'White'
-      "white-rook"
-    when type == 'Knight' && color == 'White'
-      "white-knight"
-    when type == 'Bishop' && color == 'White'
-      "white-bishop"
-    when type == 'Queen' && color == 'White'
-      "white-queen"
-    when type == 'King' && color == 'White'
-      "white-king"
-    when type == 'Pawn' && color == 'White'
-      "white-pawn"
-    end
+    "#{color}-#{type}".downcase
   end
 end
