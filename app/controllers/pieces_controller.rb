41048f0006a9a138df9a71d886ae149f75dd74ff
david <davidslee101@gmail.com>
2015-10-19 17:32:40 +0000

    add link to take user to piece.show view

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -4,21 +4,31 @@ class PiecesController < ApplicationController
   end
 
   def create
+    #put code that will create a new piece when pawn gets to
   end
 
   def new
+    #put code that will put a new piece on the board
   end
 
   def edit
+    #put code that will change a pawn to another piece
   end
 
   def show
+    @game = Game.all
+
   end
 
   def update
+    #put code that will move a piece to a new block
+    @game = Game.find(params[:id])
+    @game.update_attributes(black_player_id: current_user.id)
+    redirect_to game_path(@game)
   end
 
   def destroy
+    #put code that will kill a piece and remove it from the board
   end
 
 
