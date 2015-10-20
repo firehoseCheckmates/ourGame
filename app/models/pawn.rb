0d915a5e01066b1d825549142c1c4592909df696
david <davidslee101@gmail.com>
2015-10-20 02:51:37 +0000

    create basic legal_move method for pawn

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -6,8 +6,15 @@ class Pawn < Piece
     puts "This is a Pawn"
   end
 
-  def valid_move?
-    # put code for how this piece should move here
+  def legal_move?(row,col)
+    current_row = self.row_position
+    current_col = self.col_position
+
+    row_dif = (row - current_row)
+    col_dif = (col - current_col)
+
+    #check to see if pawn has moved one square forward, and nothing else
+    return col_dif == 1 && row_dif == 0
 
   end
 end
