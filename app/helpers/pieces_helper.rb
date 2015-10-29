70605f07bd8028bfc5339e274753a29bc534629e
david <davidslee101@gmail.com>
2015-10-29 17:04:12 +0000

    make block highlited

--- app/helpers/pieces_helper.rb
+++ app/helpers/pieces_helper.rb
@@ -1,2 +1,14 @@
 module PiecesHelper
+
+
+  def highlight(piece)
+    if current_page?(controller: 'pieces', action: 'edit', id: piece.id)
+      'highlight'
+    elsif current_page?(contoller: 'games', action: 'show', id: piece.game.id)
+      ''
+    else
+      'inactive'
+    end
+  end
 end
+
